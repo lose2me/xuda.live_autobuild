633ea7d9c1cf87133de76b9bcfbbbef2bc145410
yezerpo <Kalidium@outlook.com>
2025-09-12 19:05:24 +0800

    Update config.ts

--- src/.vuepress/config.ts
+++ src/.vuepress/config.ts
@@ -15,15 +15,4 @@ export default defineUserConfig({
   description: "旨在为徐工在校学生提供必要的生活指南",
   theme,
 
-  plugins: [
-    umamiAnalyticsPlugin({
-      id: '591b5dd1-567f-44ee-9b65-e9eae43ee773',
-      link: 'https://sta.yezer.cn/script.js',
-      autoTrack: true,
-      cache: true,
-      domains: ['xuda.live'],
-      hostUrl: 'https://sta.yezer.cn/script.js',
-    }),
-  ],
-
 });
