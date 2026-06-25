4bd88472d8beacc41585a2e77d506ea057365e2f
yezerpo <Kalidium@outlook.com>
2026-06-25 16:33:55 +0800

    fix: revert layout override, chat back in markdown

--- src/.vuepress/client.ts
+++ src/.vuepress/client.ts
@@ -1,13 +1,9 @@
 import { defineClientConfig } from "vuepress/client";
 import { defineAsyncComponent } from "vue";
 import GitHubActivity from "./components/GitHubActivity.vue";
-import Home from "./layouts/Home.vue";
 import xzitpocketConfig from "../../xzitpocket.config.json";
 
 export default defineClientConfig({
-  layouts: {
-    Home,
-  },
   enhance({ app }) {
     app.component("GitHubActivity", GitHubActivity);
     if (!__VUEPRESS_SSR__) {
